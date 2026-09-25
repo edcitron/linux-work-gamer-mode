609fa648cbdbcf6b4e41d5c0c73ae98f4c352000
Eduardo Citron <242405201+edcitron@users.noreply.github.com>
2026-09-25 18:56:45 -0300

    Add configuration for monitors and applications

---
+++ config.sh
@@ -0,0 +1,15 @@
+# Monitores
+MONITOR_PRINCIPAL="DP-4"
+MONITOR_SECUNDARIO="DP-0"
+
+# Resolução
+RESOLUCAO="1920x1080"
+
+# Frequências
+HZ_PRINCIPAL="144"
+HZ_SECUNDARIO="180"
+
+# Aplicativos
+EDITOR="code"
+NAVEGADOR="brave-browser"
+STEAM="flatpak run com.valvesoftware.Steam"

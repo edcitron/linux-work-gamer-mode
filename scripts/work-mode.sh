72abfa244a0e62340d9fb280345f1db782e39384
Eduardo Citron <242405201+edcitron@users.noreply.github.com>
2026-09-25 19:09:14 -0300

    Refactor monitor setup and improve comments
    Updated monitor configuration to use variables from config.sh and fixed comments.

--- scripts/work-mode.sh
+++ scripts/work-mode.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
+source ../config.sh
+
 # Mata as instancias do outro modo
 flatpak kill com.valvesoftware.Steam
 
-# Garantia que os programas fecharam para asa configurações de tela
+# Garantia que os programas fecharam para as configurações de tela
 sleep 1
 
 # Configura os monitores
-xrandr --output DP-4 --primary --mode 1920x1080 --rate 144 \
-       --output DP-0 --mode 1920x1080 --rate 180 \
-       --rotate left --left-of DP-4
+xrandr --output "$MONITOR_PRINCIPAL" \
+       --primary \
+       --mode "$RESOLUCAO_PRINCIPAL" \
+       --rate "$HZ_PRINCIPAL" \
+       --output "$MONITOR_SECUNDARIO" \
+       --mode "$RESOLUCAO_SECUNDARIO" \
+       --rate "$HZ_SECUNDARIO" \
+       --rotate left \
+       --left-of "$MONITOR_PRINCIPAL"
 
 # Abre os programas
-code &
-brave-browser &
+$EDITOR_CMD &
+$NAVEGADOR_CMD &
